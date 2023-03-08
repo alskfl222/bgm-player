d2b44025699ff888ea01c37fe798a73c19290c28
alsfkl222 <alskfl222@kakao.com>
2023-03-08 18:18:02 +0900

    [ FIX ] : add export command, remove comment

--- pages/stream.tsx
+++ pages/stream.tsx
@@ -59,25 +59,6 @@ export default function Stream() {
           from={queue[0].from}
           percent={percent}
         />
-        // <div
-        //   className='relative w-[600px] h-[240px] p-12 flex justify-center items-center
-        //           rounded-tr-3xl bg-neutral-700 overflow-hidden'
-        // >
-        //   <div className='w-full flex flex-col justify-center gap-6 text-neutral-100'>
-        //     <span className='max-h-[120px] text-[42px] font-bold leading-normal break-normal overflow-hidden'>
-        //       {title}
-        //     </span>
-        //     <div className='grow w-full flex justify-between items-center gap-4'>
-        //       <span className='grow text-2xl font-bold text-ellipsis whitespace-nowrap overflow-hidden'>
-        //         {queue[0].channel}
-        //       </span>
-        //       <span className='flex-none text-2xl'>{queue[0].from}</span>
-        //       <span className='flex-none w-[80px] flex justify-end text-2xl'>
-        //         {percent}%
-        //       </span>
-        //     </div>
-        //   </div>
-        // </div>
       )}
     </>
   );
